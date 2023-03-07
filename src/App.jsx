a9faf26bda6c54853c9279cfb32e0873d5ab33e0
Samad Musiliu <abdsamad2678@gmail.com>
2023-03-07 04:10:26 +0100

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function App() {
     return (
         <div className="App">
-            <h2 className="text-black font-semibold text-3xl text-center mt-4"> Yo! Wagwan </h2>
+            <h2 className="text-black font-semibold text-3xl text-center mt-4"> 3D Developer Portfolio </h2>
         </div>
     )
 }
